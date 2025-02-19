7763b62a37d378fe16f31f83abace66d6a89fd0d
Jason Goebel <goebel.jason@gmail.com>
2025-02-19 12:55:27 -0600

    Update .gitignore and modify LLM model initialization in constants.py

--- constants.py
+++ constants.py
@@ -8,8 +8,8 @@ load_dotenv()
 
 # LLM
 groq_api_key = os.getenv("GROQ_API_KEY")
-llm_model = Groq(api_key=groq_api_key)
+llm_model = Groq(model="llama3-70b-8192" ,api_key=groq_api_key)
 
 # Embeddings
 openai_api_key = os.getenv("OPENAI_API_KEY")
-embed_model = OpenAIEmbedding(model=OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE, api_key=os.getenv("OPENAI_API_KEY"))
+embed_model = OpenAIEmbedding(model=OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE, api_key=openai_api_key)
