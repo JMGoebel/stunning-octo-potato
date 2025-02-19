63e411d6df72e886c917874b7b6a70a81e39f454
Jason Goebel <goebel.jason@gmail.com>
2025-02-19 11:22:43 -0600

    Add support for Groq API and OpenAI embeddings with updated environment variables

---
+++ constants.py
@@ -0,0 +1,15 @@
+import os
+from groq import Groq
+from dotenv import load_dotenv
+from llama_index.embeddings.openai import OpenAIEmbedding, OpenAIEmbeddingModelType
+from llama_index.llms.groq import Groq
+
+load_dotenv()
+
+# LLM
+groq_api_key = os.getenv("GROQ_API_KEY")
+llm_model = Groq(api_key=groq_api_key)
+
+# Embeddings
+openai_api_key = os.getenv("OPENAI_API_KEY")
+embed_model = OpenAIEmbedding(model=OpenAIEmbeddingModelType.TEXT_EMBED_3_LARGE, api_key=os.getenv("OPENAI_API_KEY"))
